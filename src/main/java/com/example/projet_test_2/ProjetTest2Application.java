78598cdf021ce97fde12081b94c6ad99f527dcd5
oumaimaelbaggare <elbaggareoumayma@gmail.com>
2023-04-29 23:26:23 +0100

    first commit

---
+++ src/main/java/com/example/projet_test_2/ProjetTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.projet_test_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projet_test_2/ProjetTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_test_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
